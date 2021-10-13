e7dca9e17ecc81d1312af48df08e0ffb07b4a435
nlcd <danhnlc252k@gmail.com>
2021-10-13 22:07:03 +0700

    update format get / api

--- src/template.js
+++ src/template.js
@@ -1,6 +1,6 @@
 const template = `
     <div style="width:100; height:100%; display:flex; flexDirection:column; justify-content:center; align-items:center">
-        <h1 style="text-align:center; color:purple;">API<h1>
+        <h1 style="text-align:center; color:purple; display:flex; margin:auto">API<h1>
         <h2>Admission University</h2>
     </div>
 `;
