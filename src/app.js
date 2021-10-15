1e89260f6f59e1509e82f762c2924ddf829dfcde
nlcd <danhnlc252k@gmail.com>
2021-10-15 23:30:13 +0700

    add swagger-api

--- src/app.js
+++ src/app.js
@@ -3,15 +3,16 @@ const express = require('express');
 const config = require('../config/main');
 const connectdb = require('../config/dbconnection');
 const cors = require('cors');
-
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerJsDoc = YAML.load('./swagger.api.yaml');
 const { port, mongoURL } = config;
 const regionRoutes = require('./routes/region');
 const schoolRoutes = require('./routes/school');
 const template = require('./template');
-
 const app = express();
 
-cors;
+// cors
 app.use(cors());
 app.options('*', cors());
 
@@ -22,13 +23,18 @@ app.use(express.json());
 // connect db
 connectdb(mongoURL);
 
+app.use('/swagger-api', swaggerUi.serve, swaggerUi.setup(swaggerJsDoc));
+
+// default route
 app.get('/', (req, res) => {
   return res.send(template);
 });
 
+// api route
 app.use('/api/v1/regions', regionRoutes);
 app.use('/api/v1/schools', schoolRoutes);
 
+// server
 app.listen(port, () => {
   console.log(`Server running at port ${port}`);
 });
