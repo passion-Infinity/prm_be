27ff624ee44d654005d5b69cde24ddac62e56d94
nlcd <danhnlc252k@gmail.com>
2021-10-30 16:49:54 +0700

    update code api get schools to search

--- src/routes/school.js
+++ src/routes/school.js
@@ -6,12 +6,16 @@ router.get('/', async (req, res) => {
   let filter = {};
   let area = req.query.area;
   let name = req.query.name;
+  console.log('area', area);
+  console.log('name', name);
   if (area) {
-    filter = { ...filter, area };
+    filter.area = area;
   }
   if (name) {
-    filter = { ...filter, name };
+    filter.name = new RegExp(name, 'i');
   }
+
+  console.log(filter);
   const schoolList = await School.find(filter).exec();
   if (!schoolList) {
     return res.status(500).json({ code: 500 });
